f37845ec51b3630092730ee42fd71b9f903f4526
ricard <ram29f@hotmail.com>
2024-07-23 22:46:49 +0200

    update

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,14 +3,20 @@ import { getRequestContext } from '@cloudflare/next-on-pages'
 
 export const runtime = 'edge'
 
-// ...
-
 export async function GET(request: NextRequest) {
-  // this is the KV binding you defined in the wrangler.toml file
-  const myKv = getRequestContext().env.MY_KV
+  let responseText = 'Hello World'
 
-  // get a value from the namespace
-  const kvValue = await myKv.get(`kvTest`) || false
+  // In the edge runtime you can use Bindings that are available in your application
+  // (for more details see:
+  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
+  //    - https://developers.cloudflare.com/pages/functions/bindings/
+  // )
+  //
+  // KV Example:
+  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
+  // await myKv.put('suffix', ' from a KV store!')
+  // const suffix = await myKv.get('suffix')
+  // responseText += suffix
 
-  return new Response(`The value of kvTest in MY_KV is: ${kvValue}`)
+  return new Response(responseText)
 }
