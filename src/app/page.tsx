5a3b9c7cab3a55fe03cd82ae8e82940d2bee77e2
ricard <ram29f@hotmail.com>
2024-07-24 00:24:06 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,42 +3,17 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
+    
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <nav>
+        <div className={styles.wrapper}>
+          <ul className={styles.navlinks}>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+          </ul>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+      </nav>
+      <div className={styles.content}>
       <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -89,6 +64,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+      </div>
       </div>
     </main>
   );
