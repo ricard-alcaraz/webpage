b0bc89371d1bd87403a18d827c4e90e4e50dc104
ricard <ram29f@hotmail.com>
2024-07-23 19:56:50 +0200

    wrangler and next.config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 /** @type {import('next').NextConfig} */
+if (process.env.NODE_ENV === 'development') {
+    await setupDevPlatform()
+  }
+  
 const nextConfig = {};
 
 export default nextConfig;
