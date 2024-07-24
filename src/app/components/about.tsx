d46e09c02a06ec13f17d86e168950a432fbb67c0
ricard <ram29f@hotmail.com>
2024-07-24 23:58:27 +0200

    update

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -6,8 +6,8 @@ export default function About () {
     return (
         <>
             <p className={styles.name}>Ricard Alcaraz Mancebo</p>
-            <p>Hi! I'm a computer engineer with an economics master degree, I have worked</p>
-            <p>  as a Blockchain Developer and now I'm working as a Backend Developer</p>
+            <p>Hi! I&apos;m a computer engineer with an economics master degree, I have worked</p>
+            <p>  as a Blockchain Developer and now I&apos;m working as a Backend Developer</p>
         </>
     );
 }
