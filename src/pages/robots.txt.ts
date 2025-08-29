bf7739fd2780474ad99b12f4ad9fe972ef25e8f0
ricard <ram29f@hotmail.com>
2025-08-29 09:12:30 +0200

    updated robots.txt.ts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,15 +1,35 @@
+// src/pages/robots.txt.js
 import type { APIRoute } from 'astro';
 
-const getRobotsTxt = (sitemapURL: URL) => `
+const generateRobotsTxt = (sitemapURL: URL) => {
+  return `
 User-agent: *
 Allow: /
 
+User-agent: GPTBot
+Disallow:
+
+User-agent: Googlebot
+Disallow:
+
+User-agent: Bytespider
+Disallow:
+
+User-agent: Meta-ExternalAgent
+Disallow:
+
 Sitemap: ${sitemapURL.href}
-
-
-`;
+`.trim();
+};
 
 export const GET: APIRoute = ({ site }) => {
+  if (!site) {
+    return new Response('Sitemap requires a `site` configured in astro.config.mjs', { status: 500 });
+  }
   const sitemapURL = new URL('sitemap-index.xml', site);
-  return new Response(getRobotsTxt(sitemapURL));
+  return new Response(generateRobotsTxt(sitemapURL), {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  });
 };
