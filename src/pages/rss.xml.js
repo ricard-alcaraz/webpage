e3b28c6edfc97a18ebb2d126c12e95a838ed9585
Ricard Alcaraz Mancebo <ram29f@hotmail.com>
2026-09-03 10:34:24 +0200

    updated info

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 export async function GET(context) {
   return rss({
     title: 'My personal Blog | Ricard Alcaraz Mancebo',
-    description: 'Welcome to my blog, where I share my passion for frontend development, web design, and the latest technology trends.',
+    description: 'Welcome to my blog, where I share about my journey in the world of technology and analytics.',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>es</language>`,
